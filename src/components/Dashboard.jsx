006782dca6de3606f6682345f5e8a1814e3865b8
Toughee <Jimernez.io@gmail.com>
2021-08-05 17:04:30 -0500

    Min.Me/004/Component_Drafting_02 - Updated README to improve user interaction with app - Updated documentation for future app contributors - Updated node modules inside app - Committed minor code refactoring inside app - Created new favicon files for app - Drafted .json data for future component use

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,89 +1,39 @@
+import React from 'react';
 import '../App.css';
-// import ReactDOM from 'react-dom';
-// import PropTypes from 'prop-types';
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import PropTypes from 'prop-types';
+import { Button, Form } from 'react-bootstrap';
+import { FaEdit, FaTrash } from "react-icons/fa";
 
-// const newUsername = 
-// modal popup
-// input =[""] // required
-// changestate = {this.username}
+// step by step
 
+// basic div
 
-// const newPost = 
-// input = [""].required at least min=2
-//
+// input
 
-//const deletePost = 
-// onClick = delete post
+// on submit
+// include dashsection
 
+// show list
+
+// list for 
+  
 function Dashboard() {  
-    return(
-            <div>
-        <main className="DashSection">
-
-        <article className="DashStyle">
-        <input
-                    type="text"
-                />
-                <button>New post</button>
-        </article>
-
-
-{/*Create new <article> and <p> section, along with username and avatar */}
-
-{/*delete post on click */}
-
-{/*<button onClick=()>❌ </button>*/}
-{/*💓45 Likes  🌀 21 Shares  📌33 Faves */}
-
-
-            <article className="DashStyle">
-
+    return (
+        <div className="DashSection">
+      
             
-                <p><span className="DashAvatar">😶  </span>Aliquam consectetur placerat eros, 
-                consequat lobortis tellus pulvinar et. Donec bibendum erat non metus blandit, non 
-                commodo magna gravida. Donec bobe</p>
-                <span className="mediaButtons">💓45 Likes  🌀 21 Shares  📌33 Faves</span>
-            </article>
+            <input
+                type="text"
+                placeholder="Make a new post here"
+            />
+            <button>Submit</button>
 
-            <article className="DashStyle">
-                <p><span className="DashAvatar">😶  </span>consectetur adipiscing elit. Donec aliquam metus 
-                vel neque aliquam, sit amet fringilla mi consequat. Nunc egestas, mauris nec consequat tincid</p>
-                <span className="mediaButtons">11 💓 Like 45 🌀 Share 88 📌 Pin</span>
-            </article>
-
-            <article className="DashStyle">
-                <p><span className="DashAvatar">😶  </span>blandit nibh a, consectetur erat. Sed eu scelerisque mi. Aenean eget dictum urna. Duis erat tortor</p>
-                <span className="mediaButtons">93 💓 Like 54 🌀 Share 10 📌 Pin</span>
-            </article>
-
-            <article className="DashStyle">
-                <p><span className="DashAvatar">😶  </span>Aliquam consectetur placerat eros, 
-                couris nec consequat tincidunt, lorem ante aliquam arcu, Donec bibendum erat non metus blandit, non 
-                commodo magna gravida. Donec bobe</p>
-                <span className="mediaButtons">41 💓 Like 11 🌀 Share 8 📌 Pin</span>
-            </article>
-
-            <article className="DashStyle">
-                <p><span className="DashAvatar">😶  </span>Aliquam consectetummodo magna gravida. Donec id condimentum libero.
-                Cras nec leo in mi interdum dictum vitae id mi. Quisque sit amet quam sit amet diam ultrices commodo a non de</p>
-                <span className="mediaButtons">61 💓 Like 51 🌀 Share 0 📌 Pin</span>
-            </article>
-
-            <article className="DashStyle">
-                <p><span className="DashAvatar">😶  </span>Aliquam or, blandit nibh a, consectetur erat. Sed eu scelerisque mi. Aenean eget dictum urna. 
-                Duis erat tortor</p>
-                <span className="mediaButtons">15 💓 Like 14 🌀 Share 31 📌 Pin</span>
-            </article>
-
-            <article className="DashStyle">
-                <p><span className="DashAvatar">😶  </span> Donec aliquam metus vel neque aliquam, sit amet fringilla mi
-                 consequat. Nunc egestas, mauris nec consequat tincidunt, lorem ante aliquam arcu, nec laoreet ligula obe</p>
-                <span className="mediaButtons">17 💓 Like 36 🌀 Share 41 📌 Pin</span>
-            </article>
-
-            </main>
         </div>
+
     )
+                
 }
 
 export default Dashboard;
