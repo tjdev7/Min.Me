4c5ca044755eb599ddc6bd159db3852b19ac7136
Toughee <Jimernez.io@gmail.com>
2021-08-13 18:27:41 -0500

    Min.Me/005/Dependencies_Update_002 - Updated app dependencies and committed audit fixes for app - Updated app documentation - Minor refactoring for app components

---
+++ src/components/NewPost.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import props from "prop-types";
+import { FaEdit, FaTrash } from "react-icons/fa";
+
+function NewPost(props) {
+    //const edit function
+    // const [NewestPost, SetNewestPost] = useState('');
+    
+    //  function handleChange(event) {
+    //     SetNewestPost(event.target.value);
+    //  }
+  
+//    function handleSubmit(event) {
+//     event.preventDefault();
+//     //props. (props.id, NewestPost)
+//     SetNewestPost("");
+//    }
+
+   // const editpost
+
+//    <input
+//             id={props.id}
+//             value={NewestPost}
+//             onChange={handleChange}
+//         />
+
+    return(
+        <div>
+
+        <button>
+             <FaEdit/>
+            </button>
+        <input
+            id={props.id}
+
+        />
+            <button>
+                <FaTrash/>
+            </button>
+        </div>
+    );
+}
+
+
+
+
+export default NewPost;
