d267d7c51ac72cb4ad92635d0c66da6a4c2700b3
Toughee <Jimernez.io@gmail.com>
2021-12-30 10:05:09 -0600

    Fixed syntax issue with test.js script

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
 
-
 import "./App.css";
 // import ReactDOM from 'react-dom';
 // import PropTypes from 'prop-types';
--- src/App.test.js
+++ src/App.test.js
@@ -3,5 +3,4 @@ import App from "./App";
 
 test("displays front page with no error messages ", () => {
   render(<App />);
-  expect(linkElement).toBeInTheDocument();
 });
